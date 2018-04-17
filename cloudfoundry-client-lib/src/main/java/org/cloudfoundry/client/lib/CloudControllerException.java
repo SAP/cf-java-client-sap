e92dc53ea61094510c23357931468f430542f18c
Encho Belezirev <encho.belezirev@sap.com>
2018-04-17 17:08:08 +0300

    Add CloudControllerException and ServiceBrokerException

---
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/CloudControllerException.java
@@ -0,0 +1,43 @@
+package org.cloudfoundry.client.lib;
+
+import java.text.MessageFormat;
+
+import org.springframework.http.HttpStatus;
+
+public class CloudControllerException extends CloudFoundryException {
+
+    private static final long serialVersionUID = 1L;
+    private static final String DEFAULT_CLOUD_CONTROLLER_ERROR_MESSAGE = "Controller operation failed: {0}";
+
+    public CloudControllerException(CloudFoundryException cloudFoundryException) {
+        super(cloudFoundryException.getStatusCode(), cloudFoundryException.getStatusText(), cloudFoundryException.getDescription(),
+            cloudFoundryException.getCloudFoundryErrorCode(), cloudFoundryException.getCloudFoundryErrorName());
+    }
+
+    public CloudControllerException(HttpStatus statusCode) {
+        super(statusCode);
+    }
+
+    public CloudControllerException(HttpStatus statusCode, String statusText) {
+        super(statusCode, statusText);
+    }
+
+    public CloudControllerException(HttpStatus statusCode, String statusText, String description) {
+        super(statusCode, statusText, description);
+    }
+
+    public CloudControllerException(HttpStatus statusCode, String statusText, String description, int cloudFoundryErrorCode,
+        String cloudFoundryErrorName) {
+        super(statusCode, statusText, description, cloudFoundryErrorCode, cloudFoundryErrorName);
+    }
+
+    @Override
+    public String getMessage() {
+        return decorateExceptionMessage(super.getMessage());
+    }
+
+    private String decorateExceptionMessage(String exceptionMessage) {
+        return MessageFormat.format(DEFAULT_CLOUD_CONTROLLER_ERROR_MESSAGE, exceptionMessage);
+    }
+
+}
---
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/ServiceBrokerException.java
@@ -0,0 +1,42 @@
+package org.cloudfoundry.client.lib;
+
+import java.text.MessageFormat;
+
+import org.springframework.http.HttpStatus;
+
+public class ServiceBrokerException extends CloudFoundryException {
+
+    private static final long serialVersionUID = 1L;
+    private static final String DEFAULT_SERVICE_BROKER_ERROR_MESSAGE = "Service broker operation failed: {0}";
+
+    public ServiceBrokerException(CloudFoundryException cloudFoundryException) {
+        super(cloudFoundryException.getStatusCode(), cloudFoundryException.getStatusText(), cloudFoundryException.getDescription(),
+            cloudFoundryException.getCloudFoundryErrorCode(), cloudFoundryException.getCloudFoundryErrorName());
+    }
+
+    public ServiceBrokerException(HttpStatus statusCode) {
+        super(statusCode);
+    }
+
+    public ServiceBrokerException(HttpStatus statusCode, String statusText) {
+        super(statusCode, statusText);
+    }
+
+    public ServiceBrokerException(HttpStatus statusCode, String statusText, String description) {
+        super(statusCode, statusText, description);
+    }
+
+    public ServiceBrokerException(HttpStatus statusCode, String statusText, String description, int cloudFoundryErrorCode,
+        String cloudFoundryErrorName) {
+        super(statusCode, statusText, description, cloudFoundryErrorCode, cloudFoundryErrorName);
+    }
+
+    @Override
+    public String getMessage() {
+        return decorateExceptionMessage(super.getMessage());
+    }
+
+    private String decorateExceptionMessage(String exceptionMessage) {
+        return MessageFormat.format(DEFAULT_SERVICE_BROKER_ERROR_MESSAGE, exceptionMessage);
+    }
+}
