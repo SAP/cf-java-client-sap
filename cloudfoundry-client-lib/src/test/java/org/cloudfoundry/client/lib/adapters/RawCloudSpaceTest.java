d6431fde21593aba46b2d9986cb5afa7d3f97313
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-07-22 10:10:38 +0300

    Add unit tests for v3 converters + minor fixes
    JIRA: LMCROSSITXSADEPLOY-87

---
+++ cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/adapters/RawCloudSpaceTest.java
@@ -0,0 +1,43 @@
+package org.cloudfoundry.client.lib.adapters;
+
+import org.cloudfoundry.client.lib.domain.CloudSpace;
+import org.cloudfoundry.client.lib.domain.ImmutableCloudSpace;
+import org.cloudfoundry.client.v2.Resource;
+import org.cloudfoundry.client.v2.spaces.SpaceEntity;
+import org.cloudfoundry.client.v2.spaces.SpaceResource;
+import org.junit.Test;
+
+public class RawCloudSpaceTest {
+
+    private static final String NAME = "test";
+
+    @Test
+    public void testDerive() {
+        RawCloudEntityTest.testDerive(buildExpectedSpace(), buildRawSpace());
+    }
+
+    private static CloudSpace buildExpectedSpace() {
+        return ImmutableCloudSpace.builder()
+            .metadata(RawCloudEntityTest.EXPECTED_METADATA)
+            .name(NAME)
+            .build();
+    }
+
+    private static RawCloudSpace buildRawSpace() {
+        return ImmutableRawCloudSpace.of(buildTestResource());
+    }
+
+    private static Resource<SpaceEntity> buildTestResource() {
+        return SpaceResource.builder()
+            .metadata(RawCloudEntityTest.METADATA)
+            .entity(buildTestEntity())
+            .build();
+    }
+
+    private static SpaceEntity buildTestEntity() {
+        return SpaceEntity.builder()
+            .name(NAME)
+            .build();
+    }
+
+}
