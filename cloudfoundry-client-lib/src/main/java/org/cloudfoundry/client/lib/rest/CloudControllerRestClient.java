7fe9a0e90af3a6635d2ce3219b45e420ec304043
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2019-10-24 17:03:01 +0300

    Remove StagingLogs

--- cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/CloudControllerRestClient.java
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/CloudControllerRestClient.java
@@ -281,8 +281,6 @@ public interface CloudControllerRestClient {
 
     List<CloudStack> getStacks();
 
-    String getStagingLogs(StartingInfo info, int offset);
-
     List<CloudSecurityGroup> getStagingSecurityGroups();
 
     OAuth2AccessToken login();
