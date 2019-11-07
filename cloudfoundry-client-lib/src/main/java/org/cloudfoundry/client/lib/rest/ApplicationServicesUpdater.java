6ad8ad8d6007248f701b5fb2c0b92322bdcffaf3
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-11-07 15:01:23 +0200

    Small code improvements

--- cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/ApplicationServicesUpdater.java
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/ApplicationServicesUpdater.java
@@ -6,15 +6,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.cloudfoundry.client.lib.ApplicationServicesUpdateCallback;
 import org.cloudfoundry.client.lib.CloudControllerClient;
 import org.cloudfoundry.client.lib.domain.CloudApplication;
+import org.cloudfoundry.client.lib.domain.CloudEntity;
 import org.cloudfoundry.client.lib.domain.CloudServiceBinding;
 import org.cloudfoundry.client.lib.domain.CloudServiceInstance;
+import org.cloudfoundry.client.lib.util.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ApplicationServicesUpdater {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationServicesUpdater.class);
+
     private CloudControllerClient client;
 
     public ApplicationServicesUpdater(CloudControllerClient client) {
@@ -30,8 +38,7 @@ public class ApplicationServicesUpdater {
 
         List<String> deleteServices = calculateServicesToDelete(serviceNamesWithBindingParameters.keySet(), application);
 
-        List<String> rebindServices = calculateServicesToRebind(serviceNamesWithBindingParameters.keySet(),
-                                                                serviceNamesWithBindingParameters, application);
+        List<String> rebindServices = calculateServicesToRebind(serviceNamesWithBindingParameters, application);
 
         for (String serviceName : addServices) {
             client.bindService(applicationName, serviceName, serviceNamesWithBindingParameters.get(serviceName),
@@ -63,49 +70,53 @@ public class ApplicationServicesUpdater {
                           .collect(Collectors.toList());
     }
 
-    protected List<String> calculateServicesToRebind(Set<String> services,
-                                                     Map<String, Map<String, Object>> serviceNamesWithBindingParameters,
+    protected List<String> calculateServicesToRebind(Map<String, Map<String, Object>> serviceNamesWithBindingParameters,
                                                      CloudApplication application) {
         List<String> servicesToRebind = new ArrayList<>();
-        for (String serviceName : services) {
+        for (String serviceName : serviceNamesWithBindingParameters.keySet()) {
             if (!application.getServices()
                             .contains(serviceName)) {
                 continue;
             }
 
-            CloudServiceInstance cloudServiceInstance = client.getServiceInstance(serviceName);
-            Map<String, Object> newServiceBindings = getNewServiceBindings(serviceNamesWithBindingParameters, cloudServiceInstance);
-            if (hasServiceBindingsChanged(application, cloudServiceInstance, newServiceBindings)) {
-                servicesToRebind.add(cloudServiceInstance.getService()
-                                                         .getName());
+            CloudServiceInstance serviceInstance = client.getServiceInstance(serviceName);
+            Map<String, Object> newServiceBindingParameters = getNewServiceBindingParameters(serviceNamesWithBindingParameters,
+                                                                                             serviceInstance);
+            if (hasServiceBindingsChanged(application, serviceInstance, newServiceBindingParameters)) {
+                servicesToRebind.add(serviceInstance.getService()
+                                                    .getName());
             }
         }
         return servicesToRebind;
     }
 
-    private Map<String, Object> getNewServiceBindings(Map<String, Map<String, Object>> serviceNamesWithBindingParameters,
-                                                      CloudServiceInstance serviceInstance) {
+    private Map<String, Object> getNewServiceBindingParameters(Map<String, Map<String, Object>> serviceNamesWithBindingParameters,
+                                                               CloudServiceInstance serviceInstance) {
         return serviceNamesWithBindingParameters.get(serviceInstance.getService()
                                                                     .getName());
     }
 
-    private boolean hasServiceBindingsChanged(CloudApplication application, CloudServiceInstance cloudServiceInstance,
-                                              Map<String, Object> newServiceBindings) {
-        CloudServiceBinding bindingForApplication = getServiceBindingsForApplication(application, cloudServiceInstance);
-        return !Objects.equals(bindingForApplication.getBindingParameters(), newServiceBindings);
+    private boolean hasServiceBindingsChanged(CloudApplication application, CloudServiceInstance serviceInstance,
+                                              Map<String, Object> newServiceBindingParameters) {
+        CloudServiceBinding bindingForApplication = getServiceBindingForApplication(application, serviceInstance);
+        return !Objects.equals(bindingForApplication.getBindingParameters(), newServiceBindingParameters);
     }
 
-    private CloudServiceBinding getServiceBindingsForApplication(CloudApplication application, CloudServiceInstance cloudServiceInstance) {
-        return cloudServiceInstance.getBindings()
-                                   .stream()
-                                   .filter(serviceBinding -> application.getMetadata()
-                                                                        .getGuid()
-                                                                        .equals(serviceBinding.getApplicationGuid()))
-                                   .findFirst()
-                                   .orElseThrow(() -> new IllegalStateException(MessageFormat.format("Application {0} was bound to service {1} which was unbound in parallel",
-                                                                                                     application.getName(),
-                                                                                                     cloudServiceInstance.getService()
-                                                                                                                         .getName())));
+    private CloudServiceBinding getServiceBindingForApplication(CloudApplication application, CloudServiceInstance serviceInstance) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Looking for service binding between application \"{}\" and service instance \"{}\". Service bindings: {}",
+                         getGuid(application), getGuid(serviceInstance), JsonUtil.convertToJson(serviceInstance.getBindings(), true));
+        }
+        return serviceInstance.getBindings()
+                              .stream()
+                              .filter(serviceBinding -> application.getMetadata()
+                                                                   .getGuid()
+                                                                   .equals(serviceBinding.getApplicationGuid()))
+                              .findFirst()
+                              .orElseThrow(() -> new IllegalStateException(MessageFormat.format("Application {0} was bound to service {1} which was unbound in parallel",
+                                                                                                application.getName(),
+                                                                                                serviceInstance.getService()
+                                                                                                               .getName())));
     }
 
     private List<String> getUpdatedServiceNames(List<String> addServices, List<String> deleteServices, List<String> rebindServices) {
@@ -114,6 +125,11 @@ public class ApplicationServicesUpdater {
         result.addAll(deleteServices);
         result.addAll(rebindServices);
         return result;
+    }
+
+    private UUID getGuid(CloudEntity entity) {
+        return entity.getMetadata()
+                     .getGuid();
     }
 
 }
