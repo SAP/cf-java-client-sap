8ba5ad5554c1204c1367a61c1959be8a94f13a3c
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2022-10-28 09:33:48 +0300

    Improve logging for service offerings

--- src/main/java/com/sap/cloudfoundry/client/facade/Constants.java
+++ src/main/java/com/sap/cloudfoundry/client/facade/Constants.java
@@ -7,5 +7,4 @@ public class Constants {
     }
 
     public static final String PACKAGE = "package";
-    public static final String NO_SERVICE_PLAN_FOUND = "Service plan with guid \"{0}\" for service instance with name \"{1}\" was not found.";
 }
--- src/main/java/com/sap/cloudfoundry/client/facade/Messages.java
+++ src/main/java/com/sap/cloudfoundry/client/facade/Messages.java
@@ -1,8 +1,17 @@
 package com.sap.cloudfoundry.client.facade;
 
-public class Messages {
+public final class Messages {
+
+    private Messages() {
+
+    }
 
     // ERROR messages
     public static final String UNKNOWN_PACKAGE_TYPE = "Unknown package type: %s";
     public static final String CANT_CREATE_SERVICE_KEY_FOR_USER_PROVIDED_SERVICE = "Service keys can't be created for user-provided service instance \"%s\"";
+    public static final String NO_SERVICE_PLAN_FOUND = "Service plan with guid \"{0}\" for service instance with name \"{1}\" was not found.";
+    public static final String SERVICE_PLAN_WITH_GUID_0_NOT_AVAILABLE_FOR_SERVICE_INSTANCE_1 = "Service plan with guid \"{0}\" is not available for service instance \"{1}\".";
+    public static final String SERVICE_OFFERING_WITH_GUID_0_IS_NOT_AVAILABLE = "Service offering with guid \"{0}\" is not available.";
+    public static final String SERVICE_OFFERING_WITH_GUID_0_NOT_FOUND = "Service offering with guid \"{0}\" not found.";
+
 }
