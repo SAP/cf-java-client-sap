aaf44e3d92b2d1af3a5eee4c880d10b84db9d158
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2023-02-01 12:44:12 +0200

    Return logs in descending order

--- src/main/java/com/sap/cloudfoundry/client/facade/adapters/LogCacheClient.java
+++ src/main/java/com/sap/cloudfoundry/client/facade/adapters/LogCacheClient.java
@@ -1,18 +1,20 @@
 package com.sap.cloudfoundry.client.facade.adapters;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import java.util.UUID;
+
 import org.cloudfoundry.reactor.ConnectionContext;
 import org.cloudfoundry.reactor.HttpClientResponseWithBody;
 import org.cloudfoundry.reactor.TokenProvider;
 import org.cloudfoundry.reactor.util.AbstractReactorOperations;
 import org.cloudfoundry.reactor.util.JsonCodec;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.Map;
-import java.util.UUID;
 
 public class LogCacheClient extends AbstractReactorOperations {
 
@@ -35,7 +37,9 @@ public class LogCacheClient extends AbstractReactorOperations {
 
     private UriComponentsBuilder getEndpoint(UriComponentsBuilder builder, String applicationGuid) {
         return builder.pathSegment("api", "v1", "read", applicationGuid)
-                      .queryParam("envelope_types", "LOG");
+                      .queryParam("envelope_types", "LOG")
+                      .queryParam("descending", "true")
+                      .queryParam("limit", "1000");
     }
 
     private Mono<ApplicationLogsResponse> parseBody(HttpClientResponseWithBody response) {
