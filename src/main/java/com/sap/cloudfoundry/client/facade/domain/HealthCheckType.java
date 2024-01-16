feb6893c8c938541558c683679c4b7a8cab18cb2
I562548 <yavor.uzunov@sap.com>
2024-01-16 15:22:51 +0200

    add none healthcheck type
    LMCROSSITXSADEPLOY-2666

--- src/main/java/com/sap/cloudfoundry/client/facade/domain/HealthCheckType.java
+++ src/main/java/com/sap/cloudfoundry/client/facade/domain/HealthCheckType.java
@@ -1,7 +1,7 @@
 package com.sap.cloudfoundry.client.facade.domain;
 
 public enum HealthCheckType {
-    HTTP, PORT, PROCESS;
+    HTTP, PORT, PROCESS, @Deprecated NONE;
 
     @Override
     public String toString() {
